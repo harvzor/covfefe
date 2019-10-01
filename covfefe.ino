6b0483cef675fd5b2e8849227b72ebc8551b5bca
Harvey <harveyjwilliams@aol.co.uk>
2019-10-01 15:34:38 +0200

    Counts Mate too.

--- covfefe.ino
+++ covfefe.ino
@@ -6,25 +6,34 @@ LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
 void setup() {
   Serial.begin(9600);  
   Serial.println("Starting...");
-  Serial.println("Started!");
 
-  lcd.begin(16, 2);
-  lcd.setCursor(0,1);
-  lcd.write("Okay!");
+  setupLCD();
 
   setupButtons();
+
+  Serial.println("Started!");
 }
 
 unsigned int coffeeCount = 0;
+unsigned int mateCount = 0;
 
 Button buttonCoffee(2);
+Button buttonMate(3);
+
+void setupLCD() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 1);
+  lcd.write("LCD ready");
+}
 
 void setupButtons() {
   buttonCoffee.begin();
+  buttonMate.begin();
 }
 
 void readButtons() {
   buttonCoffee.read();
+  buttonMate.read();
 }
 
 void loop() {
@@ -33,6 +42,20 @@ void loop() {
   if (buttonCoffee.wasPressed()) {
     coffeeCount++;
 
-    Serial.println("coffee!  " + String(coffeeCount));
+    printOut("Coffee: " + String(coffeeCount));
   }
+
+  if (buttonMate.wasPressed()) {
+    mateCount++;
+
+    printOut("Mate: " + String(mateCount));
+  }
+}
+
+void printOut(String str) {
+  lcd.clear();
+  
+  lcd.setCursor(0, 1);
+  lcd.write(str.c_str());
+  Serial.println(str);
 }
