003da09e889baaea3f14693476fe597ec50b98e3
Harvey <harveyjwilliams@aol.co.uk>
2019-10-01 14:55:28 +0200

    Says 'Okay!'.

--- covfefe.ino
+++ covfefe.ino
@@ -1,7 +1,15 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
+
 void setup() {
   Serial.begin(9600);  
   Serial.println("Starting...");
   Serial.println("Started!");
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0,1);
+  lcd.write("Okay!");
 }
 
 void loop() {
